ca9b9251f563f227f0a7af62c5db8ceee88d898f
binhojulix <fabio.jdluz@gmail.com>
2023-01-11 00:08:11 -0300

    ansible

--- aula_inicial/scripts/jenkins.sh
+++ aula_inicial/scripts/jenkins.sh
@@ -1,12 +1,4 @@
-apt update
-apt install default-jre
-apt install default-jdk
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt update
-apt install jenkins
-#systemctl status jenkins
-ufw allow 8080
-ufw allow OpenSSH
-ufw enable
-#sudo ufw status
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add - 
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' 
+apt-get update 
+apt-get install jenkins
